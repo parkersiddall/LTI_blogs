0ec66949808b0899eb98539417b8cc9b54ede530
parkersiddall <parker.siddall@unibocconi.it>
2021-10-13 10:10:11 +0200

    launch view rendered based on role

--- app.js
+++ app.js
@@ -47,7 +47,28 @@ app.post(
   middleware_lti.validate_lti_launch,
   middleware_lti.establish_session,
   (request, response) => {
-    response.render("lti_launch", {
+
+    // render view based on user role
+    if (config.INSTRUCTOR_ROLES.includes(request.session.roles)) {
+      response.render("lti_launch", {
+        title: "You've launched!",
+        message: request.body,
+        returnUrl: request.body.launch_presentation_return_url
+      })
+    } else {
+      response.render("error", {
+        errorCode: 403,
+        errorMessage: "Your role is not authorized to see this page.",
+        returnUrl: request.body.launch_presentation_return_url
+      })
+    }
+  }
+)
+
+// test endpoint for developing pug templates
+app.get(
+  "/test", (request, response) => {
+    response.render("test", {
       title: "You've launched!",
       message: request.body,
       returnUrl: request.body.launch_presentation_return_url
--- utilities/config.js
+++ utilities/config.js
@@ -7,11 +7,24 @@ const SECRET = process.env.SECRET
 const MONGO_URL = process.env.MONGO_URL
 const SESSION_SECRET =  process.env.SESSION_SECRET
 
+// LTI roles
+const INSTRUCTOR_ROLES = [
+  "urn:lti:role:ims/lis/Instructor"
+  // add in others here once understood
+]
+
+const STUDENT_ROLES = [
+  "urn:lti:role:ims/lis/Learner"
+  // add in others here once understood
+]
+
 module.exports = {
   NODE_ENV,
   PORT,
   KEY,
   SECRET,
   MONGO_URL,
-  SESSION_SECRET
+  SESSION_SECRET,
+  INSTRUCTOR_ROLES,
+  STUDENT_ROLES
 }
